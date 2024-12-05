b8782fd048d5a183f29fd2d437dac49d76ed78c3
ssmarika <smarikashrestha02@gmail.com>
2024-12-05 21:38:44 +0545

    Update after adding image

--- CarCard/src/App.jsx
+++ CarCard/src/App.jsx
@@ -4,41 +4,49 @@ const carList = [
   {
     id: 1,
     name: "Suzuki Swift",
+    image: "src/assets/Suzuki-Swift.jpg",
     price: 3699000,
   },
   {
     id: 2,
     name: "Hyundai i20",
+    image: "src/assets/Hyundai-i20-1024x576.jpg",
     price: 3996000,
   },
   {
     id: 3,
     name: "Toyota Yaris Cross",
+    image: "src/assets/toyota-yaris-cross-.webp",
     price: 12500000,
   },
   {
     id: 4,
     name: "Kia Seltos",
+    image: "src/assets/Kia-Seltos.jpg",
     price: 5790000,
   },
   {
     id: 5,
     name: "Mahindra XUV300",
+    image: "src/assets/Mahindra-XUV300.jpg",
     price: 4275000,
   },
   {
     id: 6,
     name: "Toyota Corolla Cross",
+    image: "src/assets/toyota-corolla-cross.jpg",
     price: 13800000,
   },
   {
     id: 7,
     name: "Hyundai Creta",
+    image: "src/assets/Hyundai-Creta.jpg",
     price: 6496000,
   },
   {
     id: 8,
     name: "Tata Nexon",
+    image: "src/assets/Tata-Nexon-768x404.jpg",
     price: 4975000,
   },
 ];
@@ -63,7 +71,14 @@ const App = () => {
         CAR LISTINGS
       </h1>
       {carList.map((item, index, array) => {
-        return <Car key={item.index} name={item.name} price={item.price} />;
+        return (
+          <Car
+            key={item.index}
+            name={item.name}
+            price={item.price}
+            image={item.image}
+          />
+        );
       })}
     </div>
   );
--- CarCard/src/Car.jsx
+++ CarCard/src/Car.jsx
@@ -2,7 +2,7 @@ const Car = (props) => {
   return (
     <div
       style={{
-        width: 400,
+        width: 600,
         boxShadow:
           "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
         // gap: "5rem",
@@ -11,12 +11,28 @@ const Car = (props) => {
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
+        borderRadius: "1rem",
       }}
     >
-      <h3 style={{ color: props.price > 10000000 ? "red" : "blue" }}>
+      <img
+        src={props.image}
+        alt="image"
+        style={{
+          width: "100%",
+          height: 400,
+          borderRadius: "1rem",
+        }}
+      />
+      <h2 style={{ color: props.price > 10000000 ? "red" : "blue" }}>
         {props.name}
-      </h3>
-      <h4>{props.price}</h4>
+      </h2>
+      <h4
+        style={{
+          textDecoration: props.price > 10000000 ? "underline" : "bold",
+        }}
+      >
+        Price: {props.price}
+      </h4>
     </div>
   );
 };
